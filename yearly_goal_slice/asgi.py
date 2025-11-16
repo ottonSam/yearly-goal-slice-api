a76fed95a08156cf6f3d620f7da1559be9ff2322
Samuel Otton <ottonsam@gmail.com>
2025-11-15 22:45:34 -0300

    Initial commit

---
+++ yearly_goal_slice/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yearly_goal_slice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yearly_goal_slice.settings')
+
+application = get_asgi_application()
